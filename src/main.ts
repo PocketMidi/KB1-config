ae4f943d28a8d5703363582be893469afc334fcd
K5SMJ <sean@seanmichaeljohnston.com>
2026-04-24 16:34:58 -0500

    android color test

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Detect Android and apply color correction class
+if (/Android/i.test(navigator.userAgent)) {
+  document.body.classList.add('android-device');
+}
+
 createApp(App).mount('#app')

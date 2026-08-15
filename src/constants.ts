84c95e960cb4797e52a8a20dd0135f364dbd11c0
K5SMJ <sean@pocketmidi.com>
2026-08-15 09:22:17 -0500

    v2.3.1

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@
 /**
  * Application version (synced with firmware version)
  */
-export const APP_VERSION = '2.3.0';
+export const APP_VERSION = '2.3.1';
 
 /**
  * localStorage key for tracking if the user has seen the first-time BLE intro overlay
